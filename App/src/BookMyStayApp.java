3e730031143d28b01e72183fb39dcab78520dd2c
khaleed-m <mullamohammadk123@gmail.com>
2026-03-12 16:16:41 +0530

    [MULLA MAHAMMAD KHALEED] Added [RoomSearchAndAvailabilityCheck]

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,47 +1,26 @@
-import java.util.Map;
-
-/**
- * =====================================================
- * MAIN CLASS - UseCase3InventorySetup
- * =====================================================
- * Use Case 3: Centralized Room Inventory Management
- *
- * Demonstrates how room availability is managed
- * using a centralized inventory.
- *
- * @version 3.1
- */
-
 public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hotel Room Inventory Status\n");
+        System.out.println("Room Search\n");
 
         // Create room objects
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom = new SuiteRoom();
 
-        // Initialize inventory
+        // Create inventory
         RoomInventory inventory = new RoomInventory();
 
-        Map<String, Integer> availability = inventory.getRoomAvailability();
+        // Create search service
+        RoomSearchService searchService = new RoomSearchService();
 
-        System.out.println("Single Room:");
-        singleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + availability.get("Single"));
-
-        System.out.println();
-
-        System.out.println("Double Room:");
-        doubleRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + availability.get("Double"));
-
-        System.out.println();
-
-        System.out.println("Suite Room:");
-        suiteRoom.displayRoomDetails();
-        System.out.println("Available Rooms: " + availability.get("Suite"));
+        // Perform search
+        searchService.searchAvailableRooms(
+                inventory,
+                singleRoom,
+                doubleRoom,
+                suiteRoom
+        );
     }
 }
