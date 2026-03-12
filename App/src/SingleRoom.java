8ecb3fce7e5028b341bbcef2a369b4587383415a
khaleed-m <mullamohammadk123@gmail.com>
2026-03-12 15:51:01 +0530

    [MULLA MAHAMMAD KHALEED] Added [BasicRoomTypes&StaticAvailability]

---
+++ App/src/SingleRoom.java
@@ -0,0 +1,13 @@
+/**
+ * CLASS - SingleRoom
+ */
+
+public class SingleRoom extends Room {
+
+    /**
+     * Initializes SingleRoom with predefined attributes
+     */
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
---
+++ App/src/SuiteRoom.java
@@ -0,0 +1,13 @@
+/**
+ * CLASS - SuiteRoom
+ */
+
+public class SuiteRoom extends Room {
+
+    /**
+     * Initializes SuiteRoom with predefined attributes
+     */
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
